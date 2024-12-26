63243e9f41020f18b09890b1726dc91b5db35fec
Riju <B0295953@airtel.com>
2024-12-26 21:49:45 +0530

    Concurrency in java with lock & atomicInteger

--- Concurrency/src/Counter.java
+++ Concurrency/src/Counter.java
@@ -1,6 +1,26 @@
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Counter {
     int count = 0;
+    private final ReentrantLock lock = new ReentrantLock();
+
+    AtomicInteger atomicCount = new AtomicInteger();
+
     public synchronized void increment() {
         count++;
+    }
+
+    public void increment1() {
+        lock.lock();
+        try {
+            count++;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void increment2() {
+        atomicCount.incrementAndGet();
     }
 }
